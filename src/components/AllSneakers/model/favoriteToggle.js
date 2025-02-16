d46d80f99137e5d8dfb0ad02626ce1d9791d464e
HillTrigger <HillTrigger@proton.me>
2025-02-16 21:59:38 +0600

    Меняю состояние кнопки сразу, а не ожидаю конец запроса

--- src/components/AllSneakers/model/favoriteToggle.js
+++ src/components/AllSneakers/model/favoriteToggle.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
 export async function favoriteToggle(sneaker, likeRef) {
+  const isFavorite = sneaker.isFavorite;
   likeRef.disabled = true;
   try {
     if (!sneaker.isFavorite) {
+      sneaker.isFavorite = true;
       const newFavorite = {
         parentId: sneaker.id,
       };
@@ -11,20 +13,20 @@ export async function favoriteToggle(sneaker, likeRef) {
         'https://72f7c776150d43f2.mokky.dev/favorites',
         newFavorite,
       );
-      sneaker.isFavorite = true;
       sneaker.favoriteId = data.id;
     } else {
+      sneaker.isFavorite = false;
       const response = await axios.delete(
         `https://72f7c776150d43f2.mokky.dev/favorites/${sneaker.favoriteId}`,
       );
       if (response.status === 200 || response.status === 204) {
-        sneaker.isFavorite = false;
         sneaker.favoriteId = null;
       } else {
         console.error('Ошибка удаления избранного:', response);
       }
     }
   } catch (err) {
+    sneaker.isFavorite = isFavorite;
     console.error('Ошибка при запросе:', err);
   } finally {
     likeRef.disabled = false;
