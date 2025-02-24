29828bd6230601b07d6e4220174ee61bc3c69d28
HillTrigger <HillTrigger@proton.me>
2025-02-24 11:55:22 +0600

    3

--- src/components/AllSneakers/AllSneakers.vue
+++ src/components/AllSneakers/AllSneakers.vue
@@ -21,7 +21,7 @@ import { createOrder } from './model/createOrder';
 </script>
 
 <template>
-  <h2>все носки</h2>
+  <h2>все носки3</h2>
   <!-- <AllSneakersLayout>
     <AllSneakersTop
       @handleSortChange="(e) => handleSortChange(allSneakersSettings, e)"
